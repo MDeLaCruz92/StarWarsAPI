c9b0262fc09ebb6e8c0ceae0edb8a0b7bbeba9ed
MDeLaCruz92 <delacruzm10@gmail.com>
2017-03-27 11:43:46 -0400

    Updated UI interface

---
+++ StarWarsAPI/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  StarWarsAPI
+//
+//  Created by Michael De La Cruz on 3/27/17.
+//  Copyright © 2017 Michael De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+  
+}
