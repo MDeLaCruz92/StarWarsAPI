d90454bc8524444b9c6ea10b549ab796c5ead902
LamourBt <lamour2016@hotmail.com>
2017-04-20 01:38:39 -0400

    fixed pickerView Data

--- StarWarsAPI/CharacterVC.swift
+++ StarWarsAPI/CharacterVC.swift
@@ -16,46 +16,23 @@ protocol SearchDelegate {
 }
 
 
-
-
 class CharacterVC: UIViewController {
 
   @IBOutlet weak var characterSelection: UIPickerView!
-    @IBOutlet weak var infoView:InfoView?
-    
-//  @IBOutlet weak var bornLabel: UILabel!
-//  @IBOutlet weak var homeLabel: UILabel!
-//  @IBOutlet weak var heightLabel: UILabel!
-//  @IBOutlet weak var eyesLabel: UILabel!
-//  @IBOutlet weak var hairLabel: UILabel!
-//  @IBOutlet weak var nameLabel: UILabel!
-//  @IBOutlet weak var smallestNameLabel: UILabel!
-//  @IBOutlet weak var largestNameLabel: UILabel!
-  
-  var pickerData = [String]()
-  let person = [Person]()
-    
-
+  @IBOutlet weak var infoView:InfoView?
   @IBAction func cancelBarBtn(_ sender: UIBarButtonItem) {
-    dismiss(animated: true, completion: nil)
-  }
+     dismiss(animated: true, completion: nil)
+    }
+  var persons = [Person]()
+  var serviceModel: ServiceModel?
+
   
-    var serviceModel:ServiceModel?
-    var localDataSource: Datasource!
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    
     serviceModel = ServiceModel()
     serviceModel?.delegate = self
     serviceModel?.startTask()
- 
-    
-    self.characterSelection.delegate = self
-    self.characterSelection.dataSource = self
-
-    pickerData = ["1", "2", "3", "4", "5"]
   }
 
   
@@ -64,10 +41,10 @@ class CharacterVC: UIViewController {
 extension CharacterVC: ServiceModelDelegate {
     
     func didReceiveDataFromService(data: Datasource) {
-        //print(data.people.first)
-        localDataSource = data
-        guard let first = data.people.first  else {  fatalError() }
-        infoView?.configure(Person: first)
+        
+        self.characterSelection.delegate = self
+        self.characterSelection.dataSource = self
+        persons =  data.people
     }
 }
 
@@ -75,13 +52,11 @@ extension CharacterVC: UIPickerViewDelegate {
  
   
   func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-    return pickerData[row]
+    return  persons[row].characters
   }
   
   func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-      var selectedData = pickerData[row]
-      //infoView?.configure(Person: selectedData.)
-    
+      infoView?.configure(Person: persons[row])
   }
   
   
@@ -89,7 +64,7 @@ extension CharacterVC: UIPickerViewDelegate {
 
 extension CharacterVC:UIPickerViewDataSource {
   func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-    return pickerData.count
+    return  persons.count
   }
   
   func numberOfComponents(in pickerView: UIPickerView) -> Int {
